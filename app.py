316bf1d4b30e88fb379fa1c6810a0570dc664b81
Walter <w.estebancr@gmail.com>
2024-11-06 15:07:15 +0100

    updating the deletion feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template, flash
+from flask import Flask, request, redirect, url_for, render_template
 from data_models import db, Author, Book
 from cover_fetch import get_cover
 
@@ -29,7 +29,11 @@ def sort_func_author(book):
 @app.route('/', methods=['GET', 'POST'])
 # This route defines the home function for the home page and display the books
 def home():
-    books = Book.query.all()
+    books = []
+    try:
+        books = Book.query.all()
+    except IOError as e:
+        print("An IO error occurred: ", str(e))
     books_list = []
     for book in books:
         books_list.append({
@@ -52,9 +56,9 @@ def add_author():
     #function to add an author getting data from the add_author.html
     if request.method == 'POST':
         author = Author(
-            name=request.form.get("name"),
-            birth_date=request.form.get("birthdate"),
-            date_of_death=request.form.get("date_of_death"),
+            name=request.form["name"],
+            birth_date=request.form["birthdate"],
+            date_of_death=request.form["date_of_death"],
 
         )
         db.session.add(author) #using the flask-sqlalchemy functions to adding to the database
@@ -71,14 +75,14 @@ def add_book():
     for author in authors:
         authors_list.append(author.name)
     if request.method == 'POST':
-        author_name = request.form.get('author')
+        author_name = request.form['author']
         for author in authors:
             if author_name == author.name:
                 id_author = author.id # here we define the author_id from the author.id in authors table
         book = Book(
             title=request.form['title'],
             isbn=request.form['isbn'],
-            publication_year=request.form['publication_year'],
+            publication_year=int(request.form['publication_year']),
             author_id=id_author
         )
         db.session.add(book)
@@ -113,21 +117,23 @@ def delete(book_id):
     and deletes the author if doesn't has any other book on data base
     '''
     if request.method == 'POST':
-        book = db.session.execute(db.select(Book).filter_by(id=book_id)).scalar_one()
-        '''
-        querying the search to get the book matching with the book id from
-        the query string from the html
-        '''
-        db.session.delete(book)# deleting the book from data base
-        db.session.commit()
-        author = db.session.execute(db.select(Author).filter_by(id=book.author.id)).scalar_one()
-        books = db.session.execute(db.select(Book).filter_by(author_id=book.author_id)).scalars()
-        book_list = [book for book in books] #getting a book list from the author of the deleting book
-        if len(book_list) == 0:# checking if not has any other books
-            db.session.delete(author)# deleting the author that has no books
+        #  retrieve the book with the given id
+        book = Book.query.filter_by(id=book_id).first()
+        if book:
+            #  getting the author from the book
+            author = book.author
+            db.session.delete(book)
             db.session.commit()
+            #  check if the author is in another books
+            other_books = Book.query.filter_by(author_id=author.id).all()
+
+            if not other_books:
+                #  if there are not more books of the author, proceed to the deletion of the author
+                db.session.delete(author)
+                db.session.commit()
 
         return redirect(url_for('home'))
+    return render_template('confirm_delete.html', book_id=book_id)
 
 
 if __name__ == "__main__":
